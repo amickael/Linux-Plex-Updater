12f685976d62a653355f187e6654dced17fca575
Andrew Mickael <andrew.mickael@tdn2k.com>
2020-04-10 13:39:20 -0500

    Added install scripts

---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Ask and set variables
+install_dir=/opt/linux-plex-updater
+unit_file=linux-plex-updater.service
+
+echo "Plex username: "
+read -r -p plex_user
+
+echo "Plex password: "
+read -r -p plex_pass
+
+echo "Plex host [localhost]: "
+read -r -p plex_host
+plex_host=${plex_host:-localhost}
+
+echo "Plex port [32400]: "
+read -r -p plex_port
+plex_port=${plex_port:-32400}
+
+echo "Polling interval (seconds) [1800]: "
+read -r -p poll
+poll=${poll:-1800}
+
+
+# Set working dir
+rm -r $install_dir; mkdir $install_dir || exit
+cd $install_dir || exit
+
+# Clone code
+git clone https://github.com/amickael/Linux-Plex-Updater.git
+
+# Create virtual environment and install requirements
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
+
+# Write environment
+sed -i "s/{{CWD}}/$install_dir/g" $unit_file
+sed -i "s/{{CWD}}/$install_dir/g" environment
+sed -i "s/{{PLEX_USER}}/$plex_user/g" environment
+sed -i "s/{{PLEX_PASS}}/$plex_pass/g" environment
+sed -i "s/{{PLEX_HOST}}/$plex_host/g" environment
+sed -i "s/{{PLEX_PORT}}/$plex_port/g" environment
+sed -i "s/{{PLEX_INTERVAL}}/$poll/g" environment
+
+# Copy unit file, enable, and start
+cp $unit_file /etc/systemd/system
+systemctl enable $unit_file
+systemctl start $unit_file
